4e0bc501c5843f00fbe3d42fa6ef90e5dc4e8428
rmsham <104297535+rmsham@users.noreply.github.com>
2022-04-24 13:57:41 +0530

    Add files via upload

---
+++ SpringAPI/springboot-webservice/src/main/java/com/web/microservice/SpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.web.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebserviceApplication.class, args);
+	}
+
+}
